074c4e33ca69251ff6c84cad512907818ca0ea50
Tomasz Szubert <tomaszubert@gmail.com>
2018-02-11 10:51:18 +0100

    MAIN WITHOUT PERSON DAO

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,17 +1,13 @@
 package com.app;
 
-import com.app.dao.PersonDao;
-import com.app.dao.PersonDaoImpl;
-
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-
-        PersonDao personDao = new PersonDaoImpl();
+        System.out.println( "Hello World!" );
     }
 }
