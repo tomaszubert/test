6686b5e81638bb6a3782f1c3701cfda81ed24716
Tomasz Szubert <tomaszubert@gmail.com>
2018-02-11 10:50:27 +0100

    PERSON DAO IN MAIN CLASS

--- src/main/java/com/app/App.java
+++ src/main/java/com/app/App.java
@@ -1,5 +1,8 @@
 package com.app;
 
+import com.app.dao.PersonDao;
+import com.app.dao.PersonDaoImpl;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        PersonDao personDao = new PersonDaoImpl();
     }
 }
